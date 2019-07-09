c391d3dc839c580c2dea5c06d5c8f415e7477e1e
Meg Galvez <meg@oakmaine.com>
2019-07-09 14:32:24 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.integer "show_id"
+  end
+
+  create_table "networks", force: :cascade do |t|
+    t.integer "channel"
+    t.string "call_letters"
   end
 
   create_table "shows", force: :cascade do |t|
