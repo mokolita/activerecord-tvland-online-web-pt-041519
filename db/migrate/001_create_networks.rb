b350e40c105356baff2ee070a4f4219dacd7481c
Meg Galvez <meg@oakmaine.com>
2019-07-09 14:23:33 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_networks.rb
+++
@@ -1,8 +0,0 @@
-class CreateNetworks < ActiveRecord::Migration
-  def change
-    create_table :networks do |t|
-      t.string :call_letters
-      t.integer :channel
-    end
-  end
-end
--- db/migrate/006_add_column_to_character.rb
+++
@@ -1,7 +0,0 @@
-class CreateCharacters < ActiveRecord::Migration
-  def change
-    add_column :characters do |t| 
-      t.string :catchphrase 
-    end
-  end
-end
